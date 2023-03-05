440551bd17956054a357d0d0488053d92663a8d8
urdaydreamer <stepanenko_robot@mail.ru>
2023-03-05 16:28:27 +0300

    Переписан код для драйвера
    Функции forward(), backward() и stop() переписаны для работы с MX1508, и set_speed() изменен для того, чтобы работать с отрицательными значениями скорости при движении назад.

--- micropython/motor-speed-drive-lib.py
+++ micropython/motor-speed-drive-lib.py
@@ -1,33 +1,34 @@
 from machine import Pin, PWM
 
-class MotorController:
-    def __init__(self, enable_pin, in1_pin, in2_pin, freq=1000):
-        self.enable_pin = enable_pin
-        self.in1_pin = in1_pin
-        self.in2_pin = in2_pin
-        self.freq = freq
 
-        self.enable_pwm = PWM(Pin(enable_pin), freq=freq)
+class MotorController:
+    def __init__(self, in1_pin, in2_pin, enable_pin, freq=1000):
         self.in1_pin = Pin(in1_pin, Pin.OUT)
         self.in2_pin = Pin(in2_pin, Pin.OUT)
+        self.enable_pin = PWM(Pin(enable_pin))
+        self.enable_pin.freq(freq)
+        self.enable_pin.duty(0)
 
     def set_speed(self, speed):
-        self.enable_pwm.duty(speed)
+        if speed < 0:
+            self.in1_pin.off()
+            self.in2_pin.on()
+            speed = -speed
+        else:
+            self.in1_pin.on()
+            self.in2_pin.off()
+        self.enable_pin.duty(speed)
 
     def forward(self):
-        self.in1_pin.on()
-        self.in2_pin.off()
+        self.set_speed(1023)
 
     def backward(self):
-        self.in1_pin.off()
-        self.in2_pin.on()
+        self.set_speed(-1023)
 
     def stop(self):
-        self.in1_pin.off()
-        self.in2_pin.off()
+        self.set_speed(0)
 
 
-                #Example code:
-#motor_controller = MotorController(enable_pin=0, in1_pin=1, in2_pin=2)
+#motor_controller = MotorController(in1_pin=1, in2_pin=2, enable_pin=0)
 #motor_controller.set_speed(512)  # Set motor speed (0-1023)
 #motor_controller.forward()  # Move motor forward

e7449404652317a458b976c50fcb56b35077ba5a
urdaydreamer <stepanenko_robot@mail.ru>
2023-03-05 18:14:58 +0300

    Refactoring

--- micropython/motor-speed-drive-lib.py
+++ micropython/motor-speed-drive-lib.py
@@ -1,34 +1,81 @@
 from machine import Pin, PWM
 
-
-class MotorController:
-    def __init__(self, in1_pin, in2_pin, enable_pin, freq=1000):
-        self.in1_pin = Pin(in1_pin, Pin.OUT)
-        self.in2_pin = Pin(in2_pin, Pin.OUT)
-        self.enable_pin = PWM(Pin(enable_pin))
-        self.enable_pin.freq(freq)
-        self.enable_pin.duty(0)
-
-    def set_speed(self, speed):
-        if speed < 0:
-            self.in1_pin.off()
-            self.in2_pin.on()
-            speed = -speed
-        else:
-            self.in1_pin.on()
-            self.in2_pin.off()
-        self.enable_pin.duty(speed)
-
-    def forward(self):
-        self.set_speed(1023)
-
-    def backward(self):
-        self.set_speed(-1023)
+class MX1508:
+    def __init__(self, pin_in1, pin_in2, pin_in3, pin_in4):
+        self.pin_in1 = Pin(pin_in1, Pin.OUT)
+        self.pin_in2 = Pin(pin_in2, Pin.OUT)
+        self.pin_in3 = Pin(pin_in3, Pin.OUT)
+        self.pin_in4 = Pin(pin_in4, Pin.OUT)
+        self.pwm_a = PWM(self.pin_in1, freq=1000, duty=0)
+        self.pwm_b = PWM(self.pin_in2, freq=1000, duty=0)
+        self.pwm_c = PWM(self.pin_in3, freq=1000, duty=0)
+        self.pwm_d = PWM(self.pin_in4, freq=1000, duty=0)
+        self.direction = None
+        self.speed = 0
 
     def stop(self):
-        self.set_speed(0)
+        self.pwm_a.duty(0)
+        self.pwm_b.duty(0)
+        self.pwm_c.duty(0)
+        self.pwm_d.duty(0)
+        self.direction = None
+        self.speed = 0
+
+    def set_speed(self, speed):
+        if speed > 0:
+            self.pwm_a.duty(speed)
+            self.pwm_b.duty(0)
+            self.pwm_c.duty(speed)
+            self.pwm_d.duty(0)
+            self.direction = "forward"
+            self.speed = speed
+        elif speed < 0:
+            self.pwm_a.duty(0)
+            self.pwm_b.duty(abs(speed))
+            self.pwm_c.duty(0)
+            self.pwm_d.duty(abs(speed))
+            self.direction = "backward"
+            self.speed = abs(speed)
+        else:
+            self.stop()
+
+    def turn_left(self, speed):
+        self.pwm_a.duty(0)
+        self.pwm_b.duty(speed)
+        self.pwm_c.duty(speed)
+        self.pwm_d.duty(0)
+        self.direction = "left"
+        self.speed = speed
+
+    def turn_right(self, speed):
+        self.pwm_a.duty(speed)
+        self.pwm_b.duty(0)
+        self.pwm_c.duty(0)
+        self.pwm_d.duty(speed)
+        self.direction = "right"
+        self.speed = speed
+
+    def go_forward(self, speed):
+        self.set_speed(speed)
+        self.direction = "forward"
+
+    def go_backward(self, speed):
+        self.set_speed(-speed)
+        self.direction = "backward"
 
 
-#motor_controller = MotorController(in1_pin=1, in2_pin=2, enable_pin=0)
-#motor_controller.set_speed(512)  # Set motor speed (0-1023)
-#motor_controller.forward()  # Move motor forward
+        #Example:
+# создание экземпляра моторного драйвера
+#motor = MX1508(IN1_PIN, IN2_PIN, IN3_PIN, IN4_PIN)
+
+# движение вперед со скоростью 50
+#motor.go_forward(50)
+
+# поворот налево со скоростью 30
+#motor.turn_left(30)
+
+# движение назад со скоростью 70
+#motor.go_backward(70)
+
+# остановка моторов
+#motor.stop()

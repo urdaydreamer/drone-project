2e47ec87843c18cea165a767ff8d590e31cc96bf
urdaydreamer <stepanenko_robot@mail.ru>
2023-03-05 22:50:35 +0300

    Update main.py

--- micropython/main.py
+++ micropython/main.py
@@ -4,8 +4,10 @@ import adafruit_tcs34725
 from mx1508 import MX1508
 from tcs34725_cmyk import TCS34725_CMYK
 from vl53l0x import VL53L0X  
-
+wdt = WDT(timeout=1111)
 # создаем объекты датчиков
+timer = machine.Timer(0)
+timer.init(period=7, mode=machine.Timer.PERIODIC, callback=)
 color_recognition = TCS34725_CMYK(scl_pin=22, sda_pin=21)
 i2c = machine.I2C(scl=machine.Pin(26), sda=machine.Pin(25))
 distance_recognition = VL53L0X(i2c)
@@ -15,4 +17,5 @@ while True:
     c, m, y, k = color_recognition.read_color()
     distance = distance_recognition.read()
     print("Cyan: {:.2f}, Magenta: {:.2f}, Yellow: {:.2f}, Black: {:.2f}, Distance: {} mm".format(c, m, y, k, distance))
-    time.sleep(0.5)
+    count = sensor.get_count()
+
